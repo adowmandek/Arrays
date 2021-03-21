f2a8e2ee29c287abcef5103fb41b02b37446dc5f
adowmandek <adowmandek@gmail.com>
2021-03-21 20:58:45 +0300

    Arrays

---
+++ src/Array.kt
@@ -0,0 +1,36 @@
+
+
+fun collectionString(){
+    var namesArrays= arrayOf("mandek","hamdi","sabrin",23)
+    println(namesArrays)
+
+
+
+}
+fun citiesName() {
+    var cities=arrayOf("harare","mumbai","dodoma","jakarta")
+    var numbers= arrayOf(32,17,4,213,78,43,90,31,3,73,11,158,62)
+    var sumNumbers= arrayOf(17+78)
+    println(sumNumbers)
+
+
+
+    var numbersArray= arrayOf(158)
+    println(numbersArray)
+    var namesArray= arrayOf("Asha","Dadi","fatuma")
+    return
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
